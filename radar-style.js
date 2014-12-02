f25b19937aa785c291f420cbdba26fd13e200a5c
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-12-01 21:07:50 -0600

    made radar style have 9 pretty pictures

--- radar-style.js
+++ radar-style.js
@@ -1,139 +1,139 @@
-var textLayer = new Layer();
-var text = project.importSVG(document.getElementById('logotype'));
-text.position = new Point([400,200]);
-text.fillColor = 'white';
-//text.scale(.65);
+var Logo = function(position,displayConstruction) {
+    var textLayer = new Layer();
+    var text = project.importSVG(document.getElementById('logotype'));
+    text.position = position;
+    text.fillColor = 'white';
+    text.scale(.65);
 
-var boundsLayer = new Layer();
-var padding = 20;
-var inner = new Path.Rectangle(
-    new Point(
-	text.bounds.topLeft.x-padding,
-	text.bounds.topLeft.y-padding
-    ),
-    new Size(text.bounds.width + 2*padding, text.bounds.height + 2*padding)
-);
+    var boundsLayer = new Layer();
+    var padding = 20;
+    var inner = new Path.Rectangle(
+        new Point(
+            text.bounds.topLeft.x-padding,
+            text.bounds.topLeft.y-padding
+        ),
+        new Size(text.bounds.width + 2*padding, text.bounds.height + 2*padding)
+    );
 
-inner.strokeColor = "red";
+    inner.strokeColor = "red";
 
-console.log(text.bounds.topLeft);
+    console.log(text.bounds.topLeft);
 
-var segmentLayer = new Layer();
-// corners first
-var line_length = text.bounds.height/2;
-var corner_line_length = 10 //line_length/Math.sqrt(2);
+    var segmentLayer = new Layer();
+    // corners first
+    var line_length = text.bounds.height/2;
+    var corner_line_length = 10 //line_length/Math.sqrt(2);
 
-var tl_corner = new Path.Line(
-    inner.bounds.topLeft,
-    new Point(
-	inner.bounds.topLeft.x-corner_line_length,
-	inner.bounds.topLeft.y-corner_line_length
+    var tl_corner = new Path.Line(
+        inner.bounds.topLeft,
+        new Point(
+            inner.bounds.topLeft.x-corner_line_length,
+            inner.bounds.topLeft.y-corner_line_length
+        )
     )
-)
-var tr_corner = new Path.Line(
-    inner.bounds.topRight,
-    new Point(
-	inner.bounds.topRight.x+corner_line_length,
-	inner.bounds.topRight.y-corner_line_length
+    var tr_corner = new Path.Line(
+        inner.bounds.topRight,
+        new Point(
+            inner.bounds.topRight.x+corner_line_length,
+            inner.bounds.topRight.y-corner_line_length
+        )
     )
-)
-var tl_corner = new Path.Line(
-    inner.bounds.bottomLeft,
-    new Point(
-	inner.bounds.bottomLeft.x-corner_line_length,
-	inner.bounds.bottomLeft.y+corner_line_length
+    var tl_corner = new Path.Line(
+        inner.bounds.bottomLeft,
+        new Point(
+            inner.bounds.bottomLeft.x-corner_line_length,
+            inner.bounds.bottomLeft.y+corner_line_length
+        )
     )
-)
-var tl_corner = new Path.Line(
-    inner.bounds.bottomRight,
-    new Point(
-	inner.bounds.bottomRight.x+corner_line_length,
-	inner.bounds.bottomRight.y+corner_line_length
+    var tl_corner = new Path.Line(
+        inner.bounds.bottomRight,
+        new Point(
+            inner.bounds.bottomRight.x+corner_line_length,
+            inner.bounds.bottomRight.y+corner_line_length
+        )
     )
-)
-var left_side = new Path.Line(
-    new Point(text.position.x-(inner.bounds.width/2),
-	      text.position.y),
-    new Point(text.position.x-(inner.bounds.width/2)-line_length,
-	      text.position.y)
-)
-var right_side = new Path.Line(
-    new Point(text.position.x+(inner.bounds.width/2),
-	      text.position.y),
-    new Point(text.position.x+(inner.bounds.width/2)+line_length,
-	      text.position.y)
-)
-var horizontal_spacing = inner.bounds.width/3;
-var top_left = new Path.Line(
-    new Point(inner.bounds.topLeft.x+horizontal_spacing,
-	      inner.bounds.topLeft.y),
-    new Point(inner.bounds.topLeft.x+horizontal_spacing,
-	      inner.bounds.topLeft.y-line_length)
-)
-var top_right = new Path.Line(
-    new Point(inner.bounds.topRight.x-horizontal_spacing,
-	      inner.bounds.topRight.y),
-    new Point(inner.bounds.topRight.x-horizontal_spacing,
-	      inner.bounds.topRight.y-line_length)
-    
-)
-var bottom_left = new Path.Line(
-    new Point(inner.bounds.bottomLeft.x+horizontal_spacing,
-	      inner.bounds.bottomLeft.y),
-    new Point(inner.bounds.bottomLeft.x+horizontal_spacing,
-	      inner.bounds.bottomLeft.y+line_length)
-)
-var bottom_right = new Path.Line(
-    new Point(inner.bounds.bottomRight.x-horizontal_spacing,
-	      inner.bounds.bottomRight.y),
-    new Point(inner.bounds.bottomRight.x-horizontal_spacing,
-	      inner.bounds.bottomRight.y+line_length)
-    
-)
+    var left_side = new Path.Line(
+        new Point(text.position.x-(inner.bounds.width/2),
+                  text.position.y),
+        new Point(text.position.x-(inner.bounds.width/2)-line_length,
+                  text.position.y)
+    )
+    var right_side = new Path.Line(
+        new Point(text.position.x+(inner.bounds.width/2),
+                  text.position.y),
+        new Point(text.position.x+(inner.bounds.width/2)+line_length,
+                  text.position.y)
+    )
+    var horizontal_spacing = inner.bounds.width/3;
+    var top_left = new Path.Line(
+        new Point(inner.bounds.topLeft.x+horizontal_spacing,
+                  inner.bounds.topLeft.y),
+        new Point(inner.bounds.topLeft.x+horizontal_spacing,
+                  inner.bounds.topLeft.y-line_length)
+    )
+    var top_right = new Path.Line(
+        new Point(inner.bounds.topRight.x-horizontal_spacing,
+                  inner.bounds.topRight.y),
+        new Point(inner.bounds.topRight.x-horizontal_spacing,
+                  inner.bounds.topRight.y-line_length)
 
-var pointLayer = new Layer();
-var vertices = [];
-var p0, p1;
-segmentLayer.children.forEach(function(segment, index) {
-    segment.strokeColor = "blue";
+    )
+    var bottom_left = new Path.Line(
+        new Point(inner.bounds.bottomLeft.x+horizontal_spacing,
+                  inner.bounds.bottomLeft.y),
+        new Point(inner.bounds.bottomLeft.x+horizontal_spacing,
+                  inner.bounds.bottomLeft.y+line_length)
+    )
+    var bottom_right = new Path.Line(
+        new Point(inner.bounds.bottomRight.x-horizontal_spacing,
+                  inner.bounds.bottomRight.y),
+        new Point(inner.bounds.bottomRight.x-horizontal_spacing,
+                  inner.bounds.bottomRight.y+line_length)
 
-    // 
-    p0 = segment.segments[0].point;
-    p1 = segment.segments[1].point;
-    var vertex = p0 + (p1 - p0) * betavariate(0.35, 0.35);
-    vertices.push(vertex);
-    var p = new Path.Circle(vertex, 5);
-    p.fillColor = "rgb(150,150,255)";
+    )
 
-})
+    var pointLayer = new Layer();
+    var vertices = [];
+    var p0, p1;
+    segmentLayer.children.forEach(function(segment, index) {
+        segment.strokeColor = "blue";
 
-var polarVertices = [];
-for(var i=0; i<vertices.length; i++){
-    var polarCoords = cart_to_polar(vertices[i],inner.bounds.center);
-    polarCoords.x = vertices[i].x;
-    polarCoords.y = vertices[i].y;
-    polarVertices.push(polarCoords);
+        //
+        p0 = segment.segments[0].point;
+        p1 = segment.segments[1].point;
+        var vertex = p0 + (p1 - p0) * betavariate(0.35, 0.35);
+	vertices.push(vertex);
+        var p = new Path.Circle(vertex, 5);
+        p.fillColor = "rgb(150,150,255)";
+
+    })
+
+    var polarVertices = [];
+    for(var i=0; i<vertices.length; i++){
+        var polarCoords = cart_to_polar(vertices[i],inner.bounds.center);
+        polarCoords.x = vertices[i].x;
+        polarCoords.y = vertices[i].y;
+        polarVertices.push(polarCoords);
+    }
+    polarVertices.sort(theta_comparator);
+
+    var shapeLayer = new Layer();
+    var shape = new Path();
+    shape.fillColor = "#ff6000";
+
+    for(var i=0; i<polarVertices.length; i++){
+        shape.add(polar_to_point(polarVertices[i]));
+    }
+    shape.add(polar_to_point(polarVertices[0]));
+
+    shapeLayer.moveBelow(textLayer);
+
+    if(!displayConstruction){
+        boundsLayer.remove();
+        segmentLayer.remove();
+        pointLayer.remove();
+    }
 }
-polarVertices.sort(theta_comparator);
-
-var shapeLayer = new Layer();
-var shape = new Path();
-shape.fillColor = "#ff6000";
-
-for(var i=0; i<polarVertices.length; i++){
-    shape.add(polar_to_point(polarVertices[i]));
-}
-shape.add(polar_to_point(polarVertices[0]));
-
-shapeLayer.moveBelow(textLayer);
-
-var displayConstruction = true;
-if(!displayConstruction){
-    boundsLayer.remove();
-    segmentLayer.remove();
-    pointLayer.remove();
-}
-
 
 
 function cart_to_polar(point,center){
@@ -163,11 +163,11 @@ function gammavariate(alpha, beta) {
         var ainv = Math.sqrt(2.0 * alpha - 1.0)
         var bbb = alpha - LOG4;
         var ccc = alpha + ainv;
-	while (true) {
+        while (true) {
             var u1 = Math.random()
             if (!(1e-7 < u1 && u1 < .9999999)) {
                 continue
-	    }
+            }
             var u2 = 1.0 - Math.random();
             var v = Math.log(u1/(1.0-u1))/ainv;
             var x = alpha*Math.exp(v);
@@ -175,18 +175,18 @@ function gammavariate(alpha, beta) {
             var r = bbb+ccc*v-x;
             if(r + SG_MAGICCONST - 4.5*z >= 0.0 || r >= Math.log(z)) {
                 return x * beta
-	    }
-	}
+            }
+        }
     }
     else if (alpha === 1) {
-	var u = Math.random();
+        var u = Math.random();
         while (u <= 1e-7){
             u = Math.random();
-	}
+        }
         return -Math.log(u) * beta
     }
     else {
-	var _e = Math.exp(1);
+        var _e = Math.exp(1);
         while (true) {
             var u = Math.random();
             var b = (_e + alpha)/_e;
@@ -199,11 +199,11 @@ function gammavariate(alpha, beta) {
             if (p > 1.0) {
                 if (u1 <= Math.pow(x, (alpha - 1.0))) {
                     break;
-		}
-	    }
-	    else if (u1 <= Math.exp(-x))
+                }
+            }
+            else if (u1 <= Math.exp(-x))
                 break;
-	}
+        }
         return x * beta;
     }
 }
@@ -215,5 +215,16 @@ function betavariate(alpha, beta) {
         return y / (y + gammavariate(beta, 1.));
 }
 
+var draw = function() {
+    // this is for drawing 9 different logos on 1 canvas. we happen to know the size a priori.
+    var points = [[200,100],[600,100],[1000,100],[200,300],[600,300],[1000,300],[200,500],[600,500],[1000,500]];
+    for (var i=0; i<points.length; i++){
 
+        // put the center (?) of this SVG in this location and color it white
+
+        // make a v nice polygon
+        Logo(points[i],false);
+    }
+}
+draw();
 console.log(betavariate(.11,.11));
